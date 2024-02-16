f6bdf85f0a114774e04419c06f1fd0e518996750
KehniWind <kxn6077@mavs.uta.edu>
2024-02-15 18:17:36 -0600

    Jesus christ my computer is dying

---
+++ entity/Player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var gravity: float = 200.0
+@export var max_speed: float = 64
+@export var jump_power: float = 60
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	move()
+	apply_gravity(delta)
+	check_jump()
+	move_and_slide()
+
+func move() -> void:
+	velocity.x = Input.get_axis("left", "right") * max_speed
+
+func apply_gravity(delta: float) -> void:
+	velocity.y += gravity * delta
+
+func check_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_power
---
+++ level/Level1.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var score_label: Label = $ScoreLabel
+
+var score: int = 0
+
+func _on_area_2d_body_entered(body):
+	score += 1
+	
+	score_label.text = "Score: " + str(score)
+
+func _on_death_box_body_entered(body):
+	get_tree().reload_current_scene()
